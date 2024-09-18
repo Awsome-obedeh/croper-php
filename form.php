bc3cb4f94cc3b82f4e0c39e7050aa33624d63ce5
obed chidera <obedchidera1010@gmail.com>
2024-09-18 17:24:24 +0100

    form validation

---
+++ form.php
@@ -0,0 +1,82 @@
+<?php
+    // check if your form is submitted
+$err='';
+    if(isset($_POST['register'])){
+       echo $firstname=filter_input(INPUT_POST, 'firstname', FILTER_SANITIZE_SPECIAL_CHARS);
+        $lastname=filter_input(INPUT_POST, 'lastname', FILTER_SANITIZE_SPECIAL_CHARS);
+        $email=filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);;
+        $username=filter_input(INPUT_POST, 'username', FILTER_SANITIZE_SPECIAL_CHARS);;
+        $password=filter_input(INPUT_POST, 'password', FILTER_SANITIZE_SPECIAL_CHARS);
+
+        if(empty($firstname)){
+             $err="Please fill in firstname";
+        }
+
+        else if(empty($lastname)){
+             $err="please fill in lastname";
+        }
+
+        else if(empty($email)){
+             $err="please filll in email";
+        }
+        else if(empty($password)){
+             $err="please filll in password";
+        }
+        else if(empty($usernaae)){
+             $err="please filll in usernaame";
+        }
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- must get flowbite cdn both css and script -->
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.css"  rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.js"></script>
+</head>
+<body>
+    
+    <form class="max-w-md mx-auto mt-10" method="POST" action="<?php echo $_SERVER['PHP_SELF']?>">
+        <!-- show the error only whne the form input are not completely filled  -->
+       
+            <?php if($err) : ?> 
+                <div class="bg-red-500 text-white w-full rounded-md text-center font-semibold"> <?php echo $err ?> </div>
+           
+            <?php endif; ?>
+
+            
+    
+            
+  <div class="relative z-0 w-full mb-5 group">
+      <input  type="text" name="firstname" id="floating_email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "  />
+      <label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Firtsname</label>
+  </div>
+  <div class="relative z-0 w-full mb-5 group">
+      <input  type="text" name="lastname" id="floating_password" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "  />
+      <label for="fpassword" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">lastname</label>
+  </div>
+  <div class="relative z-0 w-full mb-5 group">
+      <input type="email" name="email" id="floating_repeat_password" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "  />
+      <label for="floating_repeat_password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email</label>
+  </div>
+  <div class="relative z-0 w-full mb-5 group">
+      <input type="text" name="username" id="floating_repeat_password" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "  />
+      <label for="floating_repeat_password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">username</label>
+  </div>
+  <div class="grid md:grid-cols-2 md:gap-6">
+    <div class="relative z-0 w-full mb-5 group">
+        <input type="password" name="password" id="floating_first_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "  />
+        <label for="floating_first_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
+    </div>
+   
+  <button name="register"type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+</form>
+
+</body>
+</html>
+
--- index.php
+++ index.php
@@ -1,72 +1,77 @@
 <?php
     $corper_name="obed";
-   $x_coper="obed";
-// in php we declear a variable using the $ sign 
-$_Globalsage=76;
+//    $x_coper="obed";
+// // in php we declear a variable using the $ sign 
+// $_Globalsage=76;
 
-// conditinals
-if($corper_name=="ajiri"){
-    echo ("you are assigned php course");  
+// // conditinals
+// if($corper_name=="ajiri"){
+//     echo ("you are assigned php course");  
+// }
+// else{
+//     echo "choose another course ";
+// }
+
+// // write a program that chehcks if a person who is ready to serve in lagost state
+// // 1. must be 18 and above
+// // 2. must not be from lagos,ondo,osun,edo
+
+// $age=18;
+// if($age<18){
+//     echo "can't server";
+// }
+// else{
+//     $state="delta";
+
+//     if($state=='lagos' || $state =="ondo"  || $state=="osun" || $state=="edo"){
+//         echo "you can't server in lagos, choose another state";
+//     }
+//     else{
+//         echo "you can serve";
+//     }
+// }
+
+// function add($a,$b){
+//     echo $a+$b;
+// }
+
+// add(5,9);
+
+// // Assignemnt
+// // write a functin to find the product of 3 numbers
+// // 2. the highest number amongst 4 parameters
+
+// echo "today is goin well" . "<br>";
+// echo "how are we";
+
+// function sub(){
+//     $a=67;
+//     echo $GLOBALS['age'];
+//     global $age;
+//     echo $age;
+
+// }
+
+// sub();
+
+// echo $age;
+// // server superglobal
+
+// // gives the location file you are in
+// echo '<br>' . $_SERVER['PHP_SELF'] .'<br>';
+// // gives the information about the server you are using
+// echo '<br>' . $_SERVER['SERVER_NAME'] .'<br>';
+
+// echo '<br>' . $_SERVER['SCRIPT_NAME'] .'<br>';
+// // prints the ip addres of the user 
+// echo '<br>' .$_SERVER['REMOTE_ADDR'] .'<br>';
+
+// listent ot when the form is submitted
+
+if($_SERVER['REQUEST_METHOD']=="POST"){
+  echo "this is the user email" .  $_REQUEST['email'] . "<br>";
+  echo "this is the user password" .  $_REQUEST['password'];
 }
-else{
-    echo "choose another course ";
-}
-
-// write a program that chehcks if a person who is ready to serve in lagost state
-// 1. must be 18 and above
-// 2. must not be from lagos,ondo,osun,edo
-
-$age=18;
-if($age<18){
-    echo "can't server";
-}
-else{
-    $state="delta";
-
-    if($state=='lagos' || $state =="ondo"  || $state=="osun" || $state=="edo"){
-        echo "you can't server in lagos, choose another state";
-    }
-    else{
-        echo "you can serve";
-    }
-}
-
-function add($a,$b){
-    echo $a+$b;
-}
-
-add(5,9);
-
-// Assignemnt
-// write a functin to find the product of 3 numbers
-// 2. the highest number amongst 4 parameters
-
-echo "today is goin well" . "<br>";
-echo "how are we";
-
-function sub(){
-    $a=67;
-    echo $GLOBALS['age'];
-    global $age;
-    echo $age;
-
-}
-
-sub();
-
-echo $age;
-// server superglobal
-
-// gives the location file you are in
-echo '<br>' . $_SERVER['PHP_SELF'] .'<br>';
-// gives the information about the server you are using
-echo '<br>' . $_SERVER['SERVER_NAME'] .'<br>';
-
-echo '<br>' . $_SERVER['SCRIPT_NAME'] .'<br>';
-// prints the ip addres of the user 
-echo '<br>' .$_SERVER['REMOTE_ADDR'] .'<br>';
-
-
 
 
 
@@ -86,13 +91,13 @@ echo '<br>' .$_SERVER['REMOTE_ADDR'] .'<br>';
 <body>
     
 
-<form class="max-w-md mx-auto" method="GET" action="index.php">
+<form class="max-w-md mx-auto" method="POST" action="index.php">
   <div class="relative z-0 w-full mb-5 group">
-      <input type="email" name="floating_email" id="floating_email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+      <input name="email" type="email" name="floating_email" id="floating_email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
       <label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
   </div>
   <div class="relative z-0 w-full mb-5 group">
-      <input type="password" name="floating_password" id="floating_password" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+      <input name="password" type="password" name="floating_password" id="floating_password" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
       <label for="floating_password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
   </div>
   <div class="relative z-0 w-full mb-5 group">
@@ -119,7 +124,7 @@ echo '<br>' .$_SERVER['REMOTE_ADDR'] .'<br>';
         <label for="floating_company" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Company (Ex. Google)</label>
     </div>
   </div>
-  <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+  <button name="register"type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
 </form>
 
 </body>
